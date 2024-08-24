b963539e8697e06bc0b0c9b698827a962cb195e3
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2024-08-24 02:59:38 +0000

    build: Updating dependencies
    composer:
    * mediawiki/mediawiki-codesniffer: 43.0.0 → 44.0.0
    * mediawiki/mediawiki-phan-config: 0.12.0 → 0.14.0
    * mediawiki/minus-x: 1.1.1 → 1.1.3
    
    npm:
    * eslint-config-wikimedia: 0.27.0 → 0.28.2
    * grunt-banana-checker: 0.11.1 → 0.13.0
    * grunt-stylelint: 0.19.0 → 0.20.1
    * stylelint-config-wikimedia: 0.16.1 → 0.17.2
    * ejs: 3.1.9 → 3.1.10
      * https://github.com/advisories/GHSA-ghr5-ch3p-vcr6
    
    Change-Id: Ifbcd36effd561255147dacf8de932965c96613c8

--- tests/selenium/specs/achievements.js
+++ tests/selenium/specs/achievements.js
@@ -4,8 +4,8 @@ const assert = require( 'assert' ),
 	AchievementsPage = require( '../pageobjects/achievements.page' ),
 	UserLoginPage = require( 'wdio-mediawiki/LoginPage' );
 
-describe( 'Special:Achievements', function () {
-	it( 'shows a logged-in user hint of long-user-page', function () {
+describe( 'Special:Achievements', () => {
+	it( 'shows a logged-in user hint of long-user-page', () => {
 		UserLoginPage.login( browser.config.mwUser, browser.config.mwPwd );
 		AchievementsPage.open();
 
