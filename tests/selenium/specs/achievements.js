bed7b7c4a389b0515e56736dd9466eee7a1916d0
Željko Filipin <zeljko.filipin@gmail.com>
2024-10-15 15:10:33 +0200

    selenium: Replace Node.js Assert library with WebdriverIO Expect library
    Assertions from Expect library are more readable that assertions from Assert library.
    
    Bug: T325740
    Change-Id: Ia95c8f2bcdad7ff10f191b9a388b0e78aaff42c6

--- tests/selenium/specs/achievements.js
+++ tests/selenium/specs/achievements.js
@@ -1,14 +1,13 @@
 'use strict';
 
-const assert = require( 'assert' ),
-	AchievementsPage = require( '../pageobjects/achievements.page' ),
-	UserLoginPage = require( 'wdio-mediawiki/LoginPage' );
+const AchievementsPage = require( '../pageobjects/achievements.page' );
+const UserLoginPage = require( 'wdio-mediawiki/LoginPage' );
 
 describe( 'Special:Achievements', () => {
 	it( 'shows a logged-in user hint of long-user-page', async () => {
 		await UserLoginPage.login( browser.config.mwUser, browser.config.mwPwd );
 		await AchievementsPage.open();
 
-		assert( await AchievementsPage.longUserPageHint.isExisting() );
+		await expect( await AchievementsPage.longUserPageHint ).toExist();
 	} );
 } );
