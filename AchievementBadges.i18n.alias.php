68397b15b3f2b1cf66cbf58bb5e70ee3971f1817
anterdc99 <xuminruixuan@live.cn>
2025-07-12 01:54:48 +0800

    AchievementBadges: Translate special page aliases into Chinese
    Change-Id: Ief8763a09e5d2b0fea66666a5118cf9d0e8e777f

--- AchievementBadges.i18n.alias.php
+++ AchievementBadges.i18n.alias.php
@@ -25,3 +25,27 @@ $specialPageAliases['ko'] = [
 	'Achievements' => [ '도전과제목록' ],
 	'ShareAchievement' => [ '도전과제공유' ],
 ];
+
+/**
+ * Chinese (中文)
+ */
+$specialPageAliases['zh'] = [
+	'Achievements' => [ 'Achievements' ],
+	'ShareAchievement' => [ 'ShareAchievement' ],
+];
+
+/*
+ * Simplified Chinese (中文（简体）)
+ */
+$specialPageAliases['zh-hans'] = [
+	'Achievements' => [ '成就' ],
+	'ShareAchievement' => [ '分享成就' ],
+];
+
+/*
+ * Traditional Chinese (中文（繁體）)
+ */
+$specialPageAliases['zh-hant'] = [
+	'Achievements' => [ '成就' ],
+	'ShareAchievement' => [ '分享成就' ],
+];
